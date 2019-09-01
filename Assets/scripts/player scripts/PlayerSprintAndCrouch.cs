3d84cf6badf41dc769ea08d70cecc29b78458829
Thibaut Colar <tcolar@colar.net>
2019-09-01 12:56:48 -0700

    Player steps sounds

--- Assets/scripts/player scripts/PlayerSprintAndCrouch.cs
+++ Assets/scripts/player scripts/PlayerSprintAndCrouch.cs
@@ -2,9 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerScrintAndCrouch : MonoBehaviour {
+public class PlayerSprintAndCrouch : MonoBehaviour {
 
     private PlayerMovement playerMovement;
+    private PlayerSteps playerSteps;
 
     public float sprintSpeed = 10;
     public float moveSpeed = 5;
@@ -15,11 +16,27 @@ public class PlayerScrintAndCrouch : MonoBehaviour {
     private float crouchHeight = 1;
 
     private bool isCrouching = false;
-    
+
+    private float sprintVol = 1f;
+    private float crouchVol = 0.1f;
+    private float walkVolMin = 0.2f;
+    private float walkVolMax = 0.6f;
+    private float walkStepDist = 0.4f;
+    private float sprintStepDist = 0.25f;
+    private float crouchStepDist = 0.5f;
+
     void Awake()
     {
         playerMovement = GetComponent<PlayerMovement>();
         lookRoot = transform.GetChild(0);
+        playerSteps = GetComponentInChildren<PlayerSteps>();
+    }
+
+    private void Start()
+    {
+        playerSteps.volMin = walkVolMin;
+        playerSteps.volMax = walkVolMax;
+        playerSteps.stepDistance = walkStepDist;
     }
 
     // Update is called once per frame
@@ -34,10 +51,16 @@ public class PlayerScrintAndCrouch : MonoBehaviour {
         if(Input.GetKeyDown(KeyCode.LeftShift) && !isCrouching)
         {
             playerMovement.speed = sprintSpeed;
+            playerSteps.stepDistance = sprintStepDist;
+            playerSteps.volMin = sprintVol;
+            playerSteps.volMax = sprintVol;
         }
         else if (Input.GetKeyUp(KeyCode.LeftShift) && !isCrouching)
         {
             playerMovement.speed = moveSpeed;
+            playerSteps.stepDistance = walkStepDist;
+            playerSteps.volMin = walkVolMin;
+            playerSteps.volMax = walkVolMin;
         }
     }
 
@@ -49,11 +72,17 @@ public class PlayerScrintAndCrouch : MonoBehaviour {
             {
                 lookRoot.localPosition = new Vector3(0f, standHeight, 0f);
                 playerMovement.speed = moveSpeed;
+                playerSteps.stepDistance = walkStepDist;
+                playerSteps.volMin = walkVolMin;
+                playerSteps.volMax = walkVolMin;
             }
             else
             {
                 lookRoot.localPosition = new Vector3(0f, crouchHeight, 0f);
                 playerMovement.speed = crouchSpeed;
+                playerSteps.stepDistance = crouchStepDist;
+                playerSteps.volMin = crouchVol;
+                playerSteps.volMax = crouchVol;
             }
             isCrouching = !isCrouching;
         }
---
+++ Assets/scripts/player scripts/PlayerSteps.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSteps : MonoBehaviour
+{
+    private CharacterController cc;
+    private AudioSource footstepSound;
+    [SerializeField]
+    private AudioClip[] footstepClip;
+    [HideInInspector]
+    public float volMin, volMax;
+    private float accDistance;
+    [HideInInspector]
+    public float stepDistance;
+
+    void Awake()
+    {
+        footstepSound = GetComponent<AudioSource>();
+        cc = GetComponentInParent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckToPlayFootsteps();
+    }
+
+    void CheckToPlayFootsteps()
+    {
+        if(!cc.isGrounded)
+        {
+            return;
+        }
+
+        if (cc.velocity.sqrMagnitude > 0)
+        {
+            accDistance += Time.deltaTime;
+            if(accDistance > stepDistance)
+            {
+                footstepSound.volume = Random.Range(volMin, volMax);
+                footstepSound.clip = footstepClip[Random.Range(0, footstepClip.Length)];
+                footstepSound.Play();
+                accDistance = 0;
+            }
+        }
+        else
+        {
+            accDistance = 0;
+        }
+    } 
+}
