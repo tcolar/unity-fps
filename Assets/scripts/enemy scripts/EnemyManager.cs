890bf5e6702de134faee3ab9808749cd79b06bd9
Thibaut Colar <tcolar@colar.net>
2019-09-02 19:57:28 -0700

    Random enemy manager

---
+++ Assets/scripts/enemy scripts/EnemyManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    public static EnemyManager instance;
+
+    [SerializeField]
+    private GameObject boarPrefab, canibalPrefab;
+    [SerializeField]
+    public int boarCount = 75;
+    //[SerializeField]
+    //private int cannibalCount, boarCount;
+    private GameObject[] boars;
+    
+    void Awake()
+    {
+        MakeInstance();
+        boars = new GameObject[boarCount];
+        for (int i=0;i!= boarCount; i++)
+        {
+            int x = Random.Range(0, 255);
+            int z = Random.Range(0, 255);
+            GameObject boar = Instantiate(boarPrefab,
+                new Vector3(x, 19, z),
+                Quaternion.identity);
+            boars[i] = boar;
+        }
+    }
+
+    void MakeInstance()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+    }
+}
